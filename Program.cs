3a9d5faf6d18066cc5ee47869cc3182cef9fbdfe
CesarRojas2019-8652 <78991630+CesarRojas2019-8652@users.noreply.github.com>
2021-03-15 06:42:55 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace practica_2_ejercicio_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] numeros = new double[10];
+            double suma = 0;
+            
+            Console.WriteLine("este programa captura los numeros los muestra y tira la suma de todos ellos.");
+
+            for (int c = 0; c < 10; c++)
+            {
+                Console.Write("Introduzca el valor {0}: ", c + 1);
+                numeros[c] = Convert.ToDouble(Console.ReadLine());
+                suma += numeros[c];
+            }
+            suma = suma / 10;
+            Console.Write("\nLos numeros introducidos fueron:\n ");
+            for (int c = 0; c < 10; c++)
+            
+            { 
+                Console.Write(" ({0}) ", numeros[c]);
+            }
+            Console.ReadKey();
+        }
+    }
+    
+}
+
